c18b0d5639f9d2a30a4eb63a5d3f95c1f695d53d
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-18 10:30:09 +0200

    initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gunther Masi Haas on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
